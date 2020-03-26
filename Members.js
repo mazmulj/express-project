b3212a599c5980fe5bec6d35b093f45969fda9bf
mazmulj <55931553+mazmulj@users.noreply.github.com>
2020-03-26 18:27:20 -0230

    Add files via upload

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Mazmul Jamil',
+        email: 'mazmul@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'James May',
+        email: 'james@gmail.com',
+        status: 'active'
+    },
+];
+
+module.exports = members;
